d710447a7a6f7a64c7f68f30b8ab0ecb409ed719
phpBBeesti <info@phpbbeesti.com>
2014-12-28 13:23:43 +0200

    Updated 12/28/2014

--- david63/cookiepolicy/language/et/cookiepolicy.php
+++ david63/cookiepolicy/language/et/cookiepolicy.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package Cookie Policy Extension [Estonian]
-* @copyright (c) 2014 david63
+* @copyright (c) 2014 david63; Estonian translation by phpBBeesti.com
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
@@ -37,29 +37,32 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'COOKIE_ACCEPT'			=> '[ Ma nõustun ]',
-	'COOKIE_ACCEPT_TEXT'	=> 'See veebilehekülg kasutab küpsiseid et pakkuda kasutajatele isikupärasemat kasutuskogemust ja mugavamat navigeerimist. Kasutades antud foorumit sa nõustud selle teabega.<br /><br />Rohkem teavet küpsiste kohta leiad kui vajutad "Küpsiste teave" linki esilehel.<br /><br />',
-	'COOKIE_POLICY'			=> 'Küpsiste teave',
+
+
+	'COOKIE_ACCEPT_TEXT'	=> 'See foorum kasutab küpsiseid, et pakkuda kasutajatele isikupärasemat kasutuskogemust, ning mugavamat navigeerimist. Kasutades antud foorumit sa nõustud selle teabega.<br /><br />Rohkem teavet küpsiste kohta leiad, kui vajutad lehekülje all olevat linki nimega “küpsiste teave”.<br /><br />',
+	'COOKIE_ACCEPT'			=> '[ Ma nõustun küpsiste teabega ]',
+
 	'COOKIE_TEXT'			=> '<h3>Kuidas me kasutame selles foorumis küpsiseid?</h3>
 
-	<p>Me kasutame küpsiseid veebilehel %1$s, et pakkuda kasutajatele isikupärasemat kasutuskogemust ja mugavamat navigeerimist. Kasutades foorumit %1$s, nõustud sellega et me paneme need faili tüübid sinu seadmesse.</p>
-<h3>Mis on küpsised?</h3><p>Küpsised on väike tekstifail, mis asub sinu arvuti või mõne muu seadme veebibrauseri ajutiste failide kaustas.<br /><br />
-Küpsistel on mitmeid erinevaid eesmärke, näiteks küpsised aitavad veebilehte ära tunda sinu seadmes järgmine kord kui sa külastad seda. %1$s kasutab terminit "küpsised" selles teabes, et viidata kõikidele asjadele, mis koguvad sellel viisil informatsiooni.<br /><br />
-Mõningad küpsised sisaldavad personaalset informatsiooni – nagu näiteks, kui vajutada linnuke "mäleta mind" kasti sisselogides, siis küpsis säilitab sinu kasutajanime. Enamik küpsiseid ei kogu ühtegi informatsiooni mis võib tuvastada teid, kuid nad koguvad pigem üldist informatsiooni nagu kuidas kasutaja saabus, kasutas veebilehekülge %1$s ja kust kohast tuli.</p>
-<h3>Missuguseid küpsiseid %1$s kasutab?</h3><p>Küpsised võivad täita mitmeid erinevaid funktsioone:</P>
-<p><b>1. Tehnilised küpsised, mis</b><br /> on vajalikud, et kasutaja saaks %1$s leheküljel üldse navigeerida ja selle funktsioone kasutada.</p>
-<p><b>2. Analüütilised küpsised, mis</b><br /> koguvad teavet selle kohta, kuidas veebilehte kasutatakse. Näiteks, milliseid lehti külastatakse kõige enam ja kas kasutajad saavad lehtedelt veateateid. Need küpsised ei kogu teavet mis võimaldaks veebilehe kasutajat otseselt tuvastada.</p>
-<p><b>3. Autentimise küpsised, mis</b><BR /> võimaldavad veebilehel meeles pidada kasutaja tehtud valikuid (näiteks nimi, keel või piirkond). Neid küpsiseid kasutavad veebilehed näiteks sisselogitud kasutaja identifitseerimiseks, kasutajad saavad end järjestikuste võrgulehtede külastuste käigus autentida ning ligi pääseda veebilehe sisule. Autentimise küpsised on enamasti ajutised küpsised.</p>
-<p>%1$s võib samuti lubada kolmandatel osapooltel kasutada küpsiseid, mis ei kuulu mitte ühtegi ülal toodud kategooriasse. Näiteks: Nagu enamus veebilehte, võib ka antud foorum kasutada "Google Analytics"\'it, et aidata monitoorida veebilehe liiklust.</p>
-<h3>Kas foorumi liikmed saavad keelata küpsiseid?</h3><p>Et saada teada kuidas hallata küpsiseid, mida sa soovid lubada vaata oma veebibrauseri abi sektsiooni või oma mobiili, tahvelarvuti kasutusjuhendit. Samuti sa võid külastada alljärgnevaid veebilehti, kus on täpsemalt kirjutatud kuidas hallata, kontrollida või kustutada küpsiseid<br /><br />
-<a href="http://www.aboutcookies.org/" style="text-decoration:none">www.aboutcookies.org</a></a><br />
-<a href="http://www.allaboutcookies.org/" style="text-decoration:none">www.allaboutcookies.org</a></a><br />
-<a href="http://www.aki.ee/et/kupsised/" style="text-decoration:none">www.aki.ee/et/kupsised</a></a></p>
-<p>Palun pea meeles et kui sa otsustad keelata küpsised, siis mõningad kohad veebilehel %1$s ei pruugi töötada korralikult.</p>
-<h3>Küpsised veebilehel %1$s sotsiaalvõrgustikke lehekülgedelt</h3><p>%1$s\'s võib olla linke sotsiaalvõrgustike veebilehtedele (nt Facebook, Twitter, Google või YouTube). Samuti antud veebilehed võivad lisada need küpsiseid sinu seadmesse, neid küpsiseid %1$s ei kontrolli, ning seega %1$s soovitab lugeda täpsemat informatsiooni kuidas antud veebilehed küpsiseid kasutavad nende endi veebilehekülgedelt.',
+	<p>Me kasutame küpsiseid veebilehel %1$s, et pakkuda kasutajatele isikupärasemat kasutuskogemust ja mugavamat navigeerimist. Kasutades antud veebilehte %1$s, sa nõustud sellega et me paneme need failitüübid sinu poolt kasutatavasse seadmesse (PC, tahvelarvuti, nutitelefon jms).</p>
+<h3>Küpsised - mis need on?</h3><p>Küpsised on väike tekstifail, mis asub sinu arvuti või mõne muu seadme veebibrauseri ajutiste failide kaustas.<br /><br />
+Küpsistel on mitmeid erinevaid eesmärke ja ülesandeid mida nad täidavad. Näiteks küpsised võimaldavad veebilehel meelde jätta kasutaja eelistusi (keele valik) või jätta vahele igakordne veebilehele sisselogimine. %1$s kasutab terminit "küpsised" selles teabes, et täpsustada kõiki üksikasju, mis viitavad just informatsiooni kogumisele sellel teel.<br /><br />
+Mõned küpsiste tüübid võivad sisaldada ka kasutajate personaalset informatsiooni - nagu näiteks, kui kasutaja  vajutab sisselogimisel "Mäleta mind" kasti linnukese, siis küpsised säilitavad tema kasutajanime. Enamik küpsiseid ei kogu ühtegi sellist informatsiooni, mis võib tuvastada teid. Kuid antud küpsised koguvad üldist informatsiooni, nagu näiteks kuidas kasutaja saabus ja kasutas veebilehekülge %1$s, ning kasutaja üldist asukohta.</p>
+<h3>Missuguseid küpsiste tüüpe kasutab %1$s?</h3><p>Küpsised võivad täita mitmeid erinevaid funktsioone, põhiolemuselt jagunevad küpsised kaheks: ajutised ehk seansi küpsised ja püsiküpsised. Sellel veebilehel kasutatakse peamiselt alljärgnevaid küpsiseid:</P>
+<p><b>1. Tehnilised küpsised, mis</b><br />on vajalikud, et kasutaja saaks veebilehel %1$s üldse navigeerida ja selle funktsioone kasutada.</p>
+<p><b>2. Analüütilised küpsised, mis</b><br />koguvad teavet selle kohta, kuidas antud veebilehekülge kasutatakse. Näiteks, milliseid lehekülgesi külastatakse kõige enam ja kas kasutajad saavad neilt veateateid. Need küpsised ei kogu teavet, mis võimaldaks veebilehekülje kasutajat otseselt tuvastada. Küll aga võib arvutikasutuse analüütiline jälgimine viia kasutaja kaudse tuvastamiseni. </p>
+<p><b>3. Autentimise küpsised, mis</b><BR />võimaldavad veebileheküljel meeles pidada kasutaja tehtud valikuid (näiteks nimi, keel või piirkond). Neid küpsiseid kasutavad veebileheküljed näiteks sisselogitud kasutaja identifitseerimiseks, kasutajad saavad end järjestikuste veebilehekülgede külastuste käigus autentida ning ligi pääseda sisule. Autentimise küpsised on enamasti ajutised küpsised. </p>
+<p>Samuti võib veebilehekülg %1$s lubada kolmandatele osapooltele kasutada küpsiseid, mis ei kuulu ühtegi üleval toodud kategooriasse. Näiteks, nagu enamus veebilehti, võib ka antud veebilehekülg kasutada Google Analytics teenust, et aidata monitoorida antud lehekülje külastatavust.</p>
+<h3>Küpsiste kasutamise piiramine või täielik loobumine</h3><p>Arvutikasutaja saab ka ise rakendada ennetavaid meetmeid, et vältida küpsiste salvestamist tema arvutisse. Selleks tuleb kasutajal muuta veebilehitseja privaatsusseadeid. Järgnevad viited juhendavad, kuidas kasutaja saab häälestada enamlevinud veebilehitsejaid küpsiste ja turvaseadete osas:<br /><br />
+<a href="http://www.aboutcookies.org" style="text-decoration:none" target="_blank">www.aboutcookies.org</a></a><br />
+<a href="http://www.allaboutcookies.org" style="text-decoration:none" target="_blank">www.allaboutcookies.org</a></a></br />
+<a href="http://www.aki.ee/et/kupsised" style="text-decoration:none" target="_blank">www.aki.ee/kupsised</a></a></p>
+<p>Siin kohal tuletame meelde, et kõikide küpsiste blokeerimine võib küll aidata kasutaja privaatsust kaitsta, kuid samas piirata ka %1$s kasutusvõimalusi.</p>
+<h3>Suhtluspluginate küpsised sisu jagamiseks veebilehel %1$s</h3><p>Veebilehekülg %1$s võib lubada ka linke sotsiaalvõrgustike veebilehtedele (näiteks: Facebook, Twitter või YouTube). Antud veebileheküljed võivad lisada omalt poolt Sinu seadmesse täiendavaid küpsiste faile, mida ei halda ega kontrolli %1$s. %1$s soovitab lugeda ka nende poolset küpsiste teavet, kuidas nemad kasutavad oma veebileheküljel küpsiseid.',
 
+	'COOKIE_POLICY'			=> 'Küpsiste teave',
 	'LOG_COOKIE_ERROR'		=> '<strong>Ebaõnnestunud IP otsing</stong>',
-	'LOG_SERVER_ERROR'		=> '<strong>Ei saanud ühendust IP server</strong>',
+	'LOG_SERVER_ERROR'		=> '<strong>Ei saanud ühendust IP serveriga</strong>',
 ));
 
 ?>
